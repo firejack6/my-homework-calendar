81c5894d1d69f1d39df5c219d4e4ec1f20b55a2e
firejack6 <jf172@uakron.edu>
2023-02-21 19:07:24 -0500

    Working calendar

--- src/calendar.js
+++ src/calendar.js
@@ -1,28 +1,35 @@
-var fs = require('fs');
-var monthsJSON = fs.readFileSync('months.json', 'utf-8');
-
-var month2num = {"Jan":1, "Feb":2, "Mar":3};
-var num2month = {1:"Jan", 2:"Feb", 3:"Mar"};
+var month2num = {"Jan":1, "Feb":2, "Mar":3, "Apr":4, "May":5, "Jun":6, "Jul":7, "Aug":8, "Sep":9, "Oct":10, "Nov":11, "Dec":12};
+var num2month = {1:"Jan", 2:"Feb", 3:"Mar", 4:"Apr", 5:"May", 6:"Jun", 7:"Jul", 8:"Aug", 9:"Sep", 10:"Oct", 11:"Nov", 12:"Dec"};
 var slideIndexLength = Object.keys(month2num).length;
 var currentMonth = "Feb";
 var numMonth = month2num[currentMonth];
 var currentYear = 2023;
 
 
-
 function mainCalendar(){
     //onload stuff
-    indexDays(); 
+    // indexDays(); 
     monthsJSON();
     // console.log(currentMonth);
 
     function monthsJSON(){
-        var rawJSON = JSON.parse(monthsJSON);
-        console.log(rawJSON);
+        bananas()
+        async function bananas(){
+            monthJSON = await fetch("months.json")
+            .then(response => response.json())
+            .then(json => monthJSON = json)
+            // console.log("calling");
+
+            var first = Object.values(monthJSON);
+            var second = first[0];
+            // console.log(second)
+            indexDays(second);
+        }
     }
 
-    function indexDays() { //creates box templates
 
+    function indexDays(second) { //creates box templates
+        // console.log("called")
         forms = document.getElementsByClassName("month");
         // console.log(forms);
         for (let l = 1; l <= slideIndexLength; l++){
@@ -47,7 +54,6 @@ function mainCalendar(){
                         dCt.classList.add("box"); //adds class
                         newDivLocation.appendChild(dCt); //adds to document
                     }
-        
             }
             currentMonthLocation.classList.add("hidden") //sets all months to be hidden by default
 
@@ -59,19 +65,69 @@ function mainCalendar(){
                 var boxDivTemp = document.createElement("div"); //creates div
                 boxDivTemp.setAttribute('id',"box" + k); //assigns id
                 var boxDivDate = document.createElement("p"); //adds text element
-                // boxDivDate.innerText = k; //testing
-
-                
-
-
+                // boxDivDate.innerText = k //box number, NOT DATE
                 boxLocation.appendChild(boxDivTemp);    //adds to doc
                 boxLocation.appendChild(boxDivDate);    //adds to doc
             }
+            setupDays(formNumber,second)
+            function setupDays(formNumber,second){
+                // console.log(monthJSON)
+                // console.log(second)
+                var twoone = second[formNumber];
+                third = twoone[0];
+                // console.log(third);
+                
+                var numDays = third["numDays"];
+                // console.log(numDays);
+                var startDay = third["startDay"]
+                // console.log(startDay)
+                setDates(formNumber, startDay, numDays);
+            
+                function setDates(formNumber,startDay,numDays){
+                    //form number sends "Jan" ,"Feb", "Mar"...
+                    var startingBox = startDay - 1;
+                    // console.log(startingBox);
+                    var check = "#box" + startingBox;
+                    // console.log(check)
+                    var startBoxId = currentMonthLocation.firstChild.querySelector(check);
+                    // console.log(startBoxId)
+                    // startBoxId.nextElementSibling.innerHTML = 1;
+                    // console.log(numDays)
+                    for (let x = 1; x <= numDays; x++){
+                        try{
+                        var date = x; //id of box
+                        // console.log(date)
+                        var boxNum = startingBox + x - 1;
+                        var dateBox = "#box" + boxNum;
+                        // console.log(dateBox)
+                        var setBox = currentMonthLocation.querySelector(dateBox);
+                        // console.log(setBox)
+                        setBox.nextElementSibling.innerHTML = date;
+                        }
+                        catch{
+                            console.log("month over")
+                        }
+                    }
+
+                    // var idk = document.createElement("p");
+                    // idk.innerText = startingBox;
+                    // boxLocation.appendChild(idk);
+                }
+            
+            }
         }
     }
-    document.getElementById(currentMonth).classList.replace("hidden","shown"); //displays current month
-    document.getElementById("monthText").innerHTML = currentMonth; //displays name of month
+    setTimeout(firstLoad, 50) // TEMPORARY SOLN
+    function firstLoad() {
+        document.getElementById(currentMonth).classList.replace("hidden","shown"); //displays current month
+        document.getElementById("monthText").innerHTML = currentMonth; //displays name of month
+    }
+    
 }
+
+
+
+
 
 function changeMonth(direction){
     var month = num2month[numMonth];
